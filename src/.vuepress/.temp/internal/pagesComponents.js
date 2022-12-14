31f8ad41803fd0090f4b197fedb7ebc6732493b0
Jiwan ghosal <jiwan.cse@gmail.com>
2022-12-14 23:33:51 +0530

    vuepress

--- src/.vuepress/.temp/internal/pagesComponents.js
+++
@@ -1,6 +0,0 @@
-import { defineAsyncComponent } from 'vue'
-
-export const pagesComponents = {
-  // path: /404.html
-  "v-3706649a": defineAsyncComponent(() => import(/* webpackChunkName: "v-3706649a" */"/Users/stchr/Documents/repo/heartstchr.github.io/src/.vuepress/.temp/pages/404.html.vue")),
-}
--- src/.vuepress/.temp/internal/pagesData.js
+++
@@ -1,4 +0,0 @@
-export const pagesData = {
-  // path: /404.html
-  "v-3706649a": () => import(/* webpackChunkName: "v-3706649a" */"/Users/stchr/Documents/repo/heartstchr.github.io/src/.vuepress/.temp/pages/404.html.js").then(({ data }) => data),
-}
