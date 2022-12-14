138ee7561e527481512f04c61aa1776d53a69e64
Jiwan ghosal <jiwan.cse@gmail.com>
2022-12-14 23:21:59 +0530

    vuepress

--- src/.vuepress/.temp/internal/clientConfigs.js
+++ src/.vuepress/.temp/internal/clientConfigs.js
@@ -1,14 +1,10 @@
-import clientConfig0 from '/Users/stchr/Documents/repo/heartstchr.github.io/src/.vuepress/.temp/sass-palette/load-hope.js'
-import clientConfig1 from '/Users/stchr/Documents/repo/heartstchr.github.io/src/.vuepress/.temp/components/config.js'
-import clientConfig2 from '/Users/stchr/Documents/repo/heartstchr.github.io/node_modules/@vuepress/plugin-active-header-links/lib/client/config.js'
-import clientConfig3 from '/Users/stchr/Documents/repo/heartstchr.github.io/node_modules/@vuepress/plugin-external-link-icon/lib/client/config.js'
+import clientConfig0 from '/Users/stchr/Documents/repo/heartstchr.github.io/node_modules/@vuepress/plugin-active-header-links/lib/client/config.js'
+import clientConfig1 from '/Users/stchr/Documents/repo/heartstchr.github.io/node_modules/@vuepress/plugin-back-to-top/lib/client/config.js'
+import clientConfig2 from '/Users/stchr/Documents/repo/heartstchr.github.io/node_modules/@vuepress/plugin-external-link-icon/lib/client/config.js'
+import clientConfig3 from '/Users/stchr/Documents/repo/heartstchr.github.io/node_modules/@vuepress/plugin-medium-zoom/lib/client/config.js'
 import clientConfig4 from '/Users/stchr/Documents/repo/heartstchr.github.io/node_modules/@vuepress/plugin-nprogress/lib/client/config.js'
 import clientConfig5 from '/Users/stchr/Documents/repo/heartstchr.github.io/node_modules/@vuepress/plugin-theme-data/lib/client/config.js'
-import clientConfig6 from '/Users/stchr/Documents/repo/heartstchr.github.io/node_modules/vuepress-plugin-copy-code2/lib/client/config.js'
-import clientConfig7 from '/Users/stchr/Documents/repo/heartstchr.github.io/src/.vuepress/.temp/md-enhance/config.js'
-import clientConfig8 from '/Users/stchr/Documents/repo/heartstchr.github.io/node_modules/vuepress-plugin-photo-swipe/lib/client/config.js'
-import clientConfig9 from '/Users/stchr/Documents/repo/heartstchr.github.io/src/.vuepress/.temp/pwa2/config.js'
-import clientConfig10 from '/Users/stchr/Documents/repo/heartstchr.github.io/src/.vuepress/.temp/theme-hope/config.js'
+import clientConfig6 from '/Users/stchr/Documents/repo/heartstchr.github.io/node_modules/@vuepress/theme-default/lib/client/config.js'
 
 export const clientConfigs = [
   clientConfig0,
@@ -18,8 +14,4 @@ export const clientConfigs = [
   clientConfig4,
   clientConfig5,
   clientConfig6,
-  clientConfig7,
-  clientConfig8,
-  clientConfig9,
-  clientConfig10,
 ]
