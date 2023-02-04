735133bc0e48a23688bf071c02085155b98252a2
Jiwan ghosal <jiwan.cse@gmail.com>
2023-02-04 18:29:32 +0530

    update project

---
+++ assets/mermaid-mindmap.esm.min-69d28910.js
@@ -0,0 +1,1 @@
+import{_ as r}from"./app-71618cce.js";import"./framework-1ec7e59d.js";const t="mindmap",i=a=>a.match(/^\s*mindmap/)!==null,o=async()=>{const{diagram:a}=await r(()=>import("./diagram-definition.0faef4c2-4dda171c.js"),[]);return{id:t,diagram:a}},e={id:t,detector:i,loader:o};export{e as default};
