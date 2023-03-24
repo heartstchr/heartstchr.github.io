9fb05dfc5314c0636f7936c7ce9a3c5aa34ad12e
Jiwan ghosal <jiwan.cse@gmail.com>
2023-03-24 14:27:44 +0530

    skills addition

---
+++ assets/mermaid-mindmap.esm.min-f1d1b544.js
@@ -0,0 +1,1 @@
+import{_ as r}from"./app-97ef4a28.js";import"./framework-c0174883.js";const t="mindmap",i=a=>a.match(/^\s*mindmap/)!==null,o=async()=>{const{diagram:a}=await r(()=>import("./diagram-definition.0faef4c2-4dda171c.js"),[]);return{id:t,diagram:a}},e={id:t,detector:i,loader:o};export{e as default};
