6e0f5a19f2616a576b49ae8bf797faddcbeab53e
Jiwan ghosal <jiwan.cse@gmail.com>
2025-04-30 12:13:02 +0530

    feat: jsdom

--- docs/.vuepress/utils/index.js
+++ docs/.vuepress/utils/index.js
@@ -1,4 +1,8 @@
-import DOMPurify from "dompurify";
+import { JSDOM } from "jsdom";
+import createDOMPurify from "dompurify";
+
+const window = new JSDOM("").window;
+const DOMPurify = createDOMPurify(window);
 
 export const sanitizedHtml = (htmlContent) => {
   return DOMPurify.sanitize(htmlContent, {
